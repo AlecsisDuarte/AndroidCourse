9a5c08014f2bed6ee47cb4d46fac2f9859b9f05c
Michael Duarte <mduarte@brooklynpacket.com>
2016-04-14 23:20:27 -0700

    added actions to buttons

--- 08_TemasEstilosGraficos/IconosYEstilosApp/app/src/main/java/mx/com/omnius/iconosyestilosapp/MainActivity.java
+++ 08_TemasEstilosGraficos/IconosYEstilosApp/app/src/main/java/mx/com/omnius/iconosyestilosapp/MainActivity.java
@@ -7,16 +7,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
 
     private Toolbar toolbar;
-
+    ImageButton button9patch, buttonNormal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button9patch = (ImageButton) findViewById(R.id.botonImagenDrawPatch);
+        button9patch.setOnClickListener(onClick);
+        buttonNormal = (ImageButton) findViewById(R.id.botonImagenNormal);
+        buttonNormal.setOnClickListener(onClick);
+    }
+
+    View.OnClickListener onClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            showToast(v==button9patch?"9PatchButton clicked": "NormalButton clicked");
+        }
+    };
+
+    void showToast(String texto){
+        Toast t = Toast.makeText(MainActivity.this.getApplicationContext(), texto, Toast.LENGTH_SHORT);
+        t.show();
     }
 
     @Override
